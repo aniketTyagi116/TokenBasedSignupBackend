70bf85c5fce174e0c9674df64d06b029b223a8db
aniketTyagi116 <97972244+aniketTyagi116@users.noreply.github.com>
2024-07-19 02:04:26 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,54 @@
+require('dotenv').config()
+
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+
+const users = []
+
+app.post('/signup', (req, res) =>{
+     const user = { name: req.body.name, password: req.body.password}
+     users.push(user)
+     res.send('success')
+})
+
+app.post('/login', (req, res) => {
+    const user = users.find(user => user.name === req.body.name);
+    if (user == null) {
+        return res.status(400).send('Cannot find user');
+    }
+
+    if (req.body.password === user.password) {
+        const accessToken = generateAccessToken(user)
+        res.json({ accessToken: accessToken})
+    } else {
+        res.send('Not Allowed');
+    }
+});
+
+app.get('/protected', authenticateToken, (req, res) => {
+    res.send('welcome')
+  })
+  
+  function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401)
+  
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      console.log(err)
+      if (err) return res.send('denied')
+      req.user = user
+      next()
+    })
+  }
+
+function generateAccessToken(user) {
+    return jwt.sign(user,process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+  }
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000')
+});
